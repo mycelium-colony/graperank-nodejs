8e8a20b2608a4b556670d909b298ad9e4d6e2057
manimejia <noreply@manimejia.me>
2024-09-13 10:33:38 -0500

    added influence calculations in pseudo code
    via pair programming

--- typescript/CalcScorecards.ts
+++ typescript/CalcScorecards.ts
@@ -6,6 +6,8 @@ type context = string
 /**
  * A Scorecards Table 
  * Input and output from GrapeRank calculations
+ * pubkey = observer
+ * uid = being observed
  */
 type G = Record< 
   pubkey, Record< 
@@ -23,18 +25,29 @@ class Scorecard {
 }
 
 /**
- * A Ratings Table
+ * A Ratings Table per Event Kind for ALL of Nostr
  * Input for GrapeRank calculations
+ * pubkey = rater
+ * uid = being rated
  */
 type R = Record< 
   pubkey, Record< 
-    uid, Rating >>
+    uid, Rating | undefined >>
 
 class Rating {
   confidence : number // 0 - 1
-  score : number // number
+  score : number // 0 or 1 / percent
   context? : string // TODO Rating might need to be indexed by context
 }
+
+type APIRatingsParams = {
+  pubkeys : pubkey[]
+  // observer : pubkey
+  kinds : number[]
+  // confidence : number // 0 - 1
+  // scoreModifierCallback : Function
+}
+
 
 /**
  * Observer Parameters for GrapeRank
@@ -51,7 +64,58 @@ class P {
 }
 
 
-function calculateGrapevineTable( G : G, R : R, P : P) : G {
+
+function APIrequest(APIRatingsParams) : R[] {
+  return {}[]
+}
+
+
+// FIXME PSEUDO CODE !!
+function calculateRatings(events : any[], params : {}):R{
+  let R_out : R = {}
+  for(let event in events){
+    let kind = event.kind
+    let author : pubkey = event.pubkey
+    let callback : Function | undefined = undefined;
+    let authorRatings = {}
+    switch(kind){
+      case 3 : callback = getAuthorRatingForEventKind_3
+      case 1984 : callback = getAuthorRatingForEventKind_1984
+      // case 10000 : callback = getRatingForEventKind_10000
+      break
+    }
+    if(callback) R_out[author] = {...authorRatings, ...callback(event)}
+  }
+  return R_out
+}
+
+function getAuthorRatingForEventKind_3(event) : {[k:uid]:Rating} {
+  let follows : pubkey[] = getFollowsFromKind3(event)
+  let authorRatings = {}
+  for(let pubkey in follows){
+    authorRatings[pubkey] = followRating
+  }
+  return authorRatings
+}
+
+
+function getAuthorRatingForEventKind_1984(event) :  {[k:uid]:Rating} {
+  let reported : pubkey = getTagFromEvent(event, "p")
+  R_out[author][reported] = reportRating
+}
+
+
+
+
+
+type GID = [pubkey?,uid?,context?] 
+
+// return G from cache using GID
+function getScorecardsFromCache(gid: GID) : G {
+    return {}
+}
+
+function calculateScorecards( G : G, R : R, P : P) : G {
     // const g_out = new G()
     let rated : {[x:uid] : Scorecard} = {}
     let raters = {}
